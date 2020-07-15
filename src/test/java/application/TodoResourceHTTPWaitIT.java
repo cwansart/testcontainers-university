6643eae5d4add993edec3802de8913cbaf25e632
max.weis <max.weis@openknowledge.de>
2020-07-15 08:05:26 +0200

    move exercise 3 text to master

--- src/test/java/application/TodoResourceHTTPWaitIT.java
+++ src/test/java/application/TodoResourceHTTPWaitIT.java
@@ -21,17 +21,6 @@ import java.io.File;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeoutException;
 
-/**
- * EXERCISE 3.1: Use HTTP wait strategy to wait for the service
- * HOWTO:
- * 1. Find the health check of the service
- * 2. Add the waitingFor() method to the container builder
- * 3. Inside the waitingFor() method add the Wait object with the correct wait strategies
- *
- * *** tips ***
- * - you should wait for a http endpoint
- * - you should check the status code
- */
 @RunWith(JUnit4.class)
 public class TodoResourceHTTPWaitIT {
 
